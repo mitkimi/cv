a79a9e9ecb1a5c1309f93b38e07554f32bd0638f
田昊天 <h@otian.me>
2020-09-18 23:24:17 +0800

    add, 增加了架构能力和管理能力

--- src/data/professionalSkills.js
+++ src/data/professionalSkills.js
@@ -1,7 +1,15 @@
 const professionalSkills = [
   {
+    name: '架构能力',
+    desc: '通读过 React.js/Vue.js 的代码，基本了解框架的实现原理，可以根据需求快速的搭建程序框架。对不同类别的工具或插件进行过对比，能根据需求快速搭建出符合情况并且具有扩展性的架构。'
+  },
+  {
+    name: '管理能力',
+    desc: '带过 8 人团队，给 100 人团队的 CTO 当过副手。在我的带领下，我组成员在两个月内在平均工作量少量上浮的情况下从每天加班两小时还无法完成工作的情况下提升到不加班即可完成，并且还有时间开组会、培训会等。'
+  },
+  {
     name: '工作平台',
-    desc: '熟练使用Mac、Linux GUI和Windows进行开发，熟悉H5，CSS3，熟练使用Bootstrap、jQuery、jQuery.ajax。对React.js、Vue.js情有独钟，参与过多个SaaS、系统的开发。'
+    desc: '熟练使用 Mac 进行开发，熟悉H5，CSS3，对React.js、Vue.js情有独钟，参与过多个SaaS、系统的开发。'
   },
   {
     name: '联合开发能力',
